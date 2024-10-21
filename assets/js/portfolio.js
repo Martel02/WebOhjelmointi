17d41c05ea12762e2d7377b09ab262a60521d6cb
Martel <t2hema02@students.oamk.fi>
2024-10-21 19:10:57 +0300

    yes

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -27,6 +27,6 @@ function countDown() {
 }
 
 function loadPage() {
-    const parsehtml = new DOMParser().parseFromString('<iframe width="1280" height="720" src="https://www.youtube.com/embed/dQw4w9WgXcQ?si=vhSt_cqFf9M1cqk3&amp;controls=0&autoplay=1" title="YouTube video player" frameborder="0" allowfullscreen></iframe>', "text/html");
+    const parsehtml = new DOMParser().parseFromString('<iframe width="1280" height="720" src="https://www.youtube.com/embed/dQw4w9WgXcQ?si=vhSt_cqFf9M1cqk3&amp;controls=0&autoplay=1" title="YouTube video player" frameborder="0" allow="autoplay;" allowfullscreen></iframe>', "text/html");
     document.body.appendChild(parsehtml.body.lastChild);
 }
